a0ff7cfc850fe73a22359fdc9a170b8f7ea13525
Natalia392 <ntorrejonvalencia@gmail.com>
2023-10-25 15:30:19 -0300

    natalia feat: embed chat in Home

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,21 @@
+import { BubbleChat } from "flowise-embed-react";
 import FormBot from "./components/form"
 
 const Home = () => (
-    <><section className='Home'>
-    <h1>HOLA</h1>
-  </section><FormBot /></>
-  );
-  
-  export default Home;
-  
+  <>
+    <section className='Home'>
+      <h1>HOLA</h1>
+      <FormBot />
+    </section>
+    <section>
+      <BubbleChat
+        chatflowid="233012fc-31f3-4452-b3a0-7e83ec692e6e"
+        apiHost="https://itau-2-tf.onrender.com"
+        bubbleColor="#0078d4" // Cambiar el color de los bocadillos de chat
+        avatarSize={50} />
+    </section>
+  </>
+);
+
+export default Home;
+
