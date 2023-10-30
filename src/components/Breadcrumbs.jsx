94670312aa4f42c30d928723fcd47ab3d3d097d1
orianajpp <orianajpp@gmail.com>
2023-10-30 12:28:10 -0300

    pruebas nombre de archivos

--- src/components/Breadcrumbs.jsx
+++ src/components/Breadcrumbs.jsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from 'react-router-dom';
-import styles from './Breadcrumbs.module.css';
+import styles from './breadcrumbs.module.css';
 
 const Breadcrumbs = () => {
     const location = useLocation();
